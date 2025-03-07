2ae403e154d769141b93d37e3677c0e0b0e30e41
Mahmood <mahmoodsalari2005@gmail.com>
2025-03-07 11:17:27 +0330

    E

--- The_Wild_Oasis/src/services/supabase.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabaseUrl = "https://whrmhbvmxnhlmmoasqic.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indocm1oYnZteG5obG1tb2FzcWljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA5MjY1NTMsImV4cCI6MjA1NjUwMjU1M30.cPNOT2GPqA2NEkEhBmANWsBoEojVRPyvRgnvtFOo3Qc";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
-
